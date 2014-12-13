cb3e8375422538f05ea80af914fca413c980249e
psgs <fpigerre@gmail.com>
2014-12-13 17:13:03 +1000

    Disable registrations
    Disable the registrations page via registration "new" and "create"
    actions. Perhaps add a production environment check later? #TODO

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:sessions => 'sessions'}, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
+  devise_for :users, :controllers => {:sessions => 'sessions', :registrations => 'registrations'}, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
